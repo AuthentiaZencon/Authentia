c421b579383e2cb313aa674608d50706f780d8c3
Jose Bayona <bayonaj368@gmail.com>
2023-09-19 14:59:11 -0600

    feat: upload images

--- app/dash/layout.tsx
+++ app/dash/layout.tsx
@@ -3,13 +3,13 @@ import { Sidebar } from "./register/components"
 export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
-        <main className="w-full h-screen">
+        <main className="w-full h-screen flex">
 
-            <aside className="hidden md:block w-full md:w-64 h-full border-r border-[#3B3E3C]">
+            <aside className="hidden md:block w-full md:w-80 h-full border-r border-[#3B3E3C]">
                 <Sidebar />
             </aside>
 
-            <aside className="w-full md:w-[calc(256px-100%)] h-full">
+            <aside className="w-full h-full pt-16 px-14">
                 {children}
             </aside>
 
